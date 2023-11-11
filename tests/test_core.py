efeb2130a64698ee0db8692f3ee085eeef80fb69
Jannes <jannes.hachmer@gmail.com>
2023-11-11 21:46:55 +0100

    Updated Tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,130 +1,160 @@
-# Desc: Unit tests for core.py
 # flake8: noqa
-from unittest.mock import patch
+import os
+from unittest.mock import MagicMock, patch
 
 import ezsheets
 import pytest
 import requests
 
-from sheepy.core import find_free_row, get_movie_data
+from sheepy.core import (
+    add_to_sheet,
+    extract_movie_data,
+    find_free_row,
+    find_movie_sheet,
+    get_movie_data,
+    get_spreadsheet_and_sheet,
+    read_user_cli_args,
+)
+
+SUGGESTED_BY = os.environ.get("SUGGESTED_BY", "Someone")
 
 
-@patch("sheepy.core.requests.get")
-def test_get_movie_data_success(mock_get):
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.json.return_value = {
-        "Title": "The Matrix",
-        "Year": "1999",
-        "Rated": "R",
-        "Released": "31 Mar 1999",
-        "Runtime": "136 min",
-        "Genre": "Action, Sci-Fi",
-        "Director": "Lana Wachowski, Lilly Wachowski",
-        "Writer": "Lilly Wachowski, Lana Wachowski",
-        "Actors": "Keanu Reeves, Laurence Fishburne, " "Carrie-Anne Moss, Hugo Weaving",
-        "Plot": "A computer hacker learns from mysterious "
-        "rebels about the true nature of his reality "
-        "and his role in the war against its controllers.",
-        "Language": "English",
-        "Country": "USA",
-        "Awards": "Won 4 Oscars. Another 37 wins & 51 nominations.",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BMjAxMjg5ODI4N15BMl5BanBnXkFtZTYwNjM5Mzg5._V1_SX300.jpg",
+@pytest.fixture
+def imdb_id():
+    return "tt1375666"
+
+
+@pytest.fixture
+def movie_data():
+    return {
+        "Title": "Inception",
+        "Year": "2010",
+        "Rated": "PG-13",
+        "Released": "16 Jul 2010",
+        "Runtime": "148 min",
+        "Genre": "Action, Adventure, Sci-Fi",
+        "Director": "Christopher Nolan",
+        "Writer": "Christopher Nolan",
+        "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+        "Plot": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+        "Language": "English, Japanese, French",
+        "Country": "USA, UK",
+        "Awards": "Won 4 Oscars. Another 152 wins & 218 nominations.",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMjAxMjA5MjgxNF5BMl5BanBnXkFtZTcwMjQyMjIyMw@@._V1_SX300.jpg",
         "Ratings": [
-            {"Source": "Internet Movie Database", "Value": "8.7/10"},
-            {"Source": "Rotten Tomatoes", "Value": "88%"},
-            {"Source": "Metacritic", "Value": "73/100"},
+            {"Source": "Internet Movie Database", "Value": "8.8/10"},
+            {"Source": "Rotten Tomatoes", "Value": "87%"},
+            {"Source": "Metacritic", "Value": "74/100"},
         ],
-        "Metascore": "73",
-        "imdbRating": "8.7",
-        "imdbVotes": "1,719,757",
-        "imdbID": "tt0133093",
+        "Metascore": "74",
+        "imdbRating": "8.8",
+        "imdbVotes": "2,097,177",
+        "imdbID": "tt1375666",
         "Type": "movie",
-        "DVD": "21 Sep 1999",
-        "BoxOffice": "$171,479,930",
-        "Production": "Village Roadshow Prod., Silver Pictures",
+        "DVD": "07 Dec 2010",
+        "BoxOffice": "$292,576,195",
+        "Production": "Syncopy, Warner Bros.",
         "Website": "N/A",
         "Response": "True",
     }
-    result = get_movie_data("tt0133093")
-    assert result["Title"] == "The Matrix"
-    assert result["Year"] == "1999"
-    assert result["Rated"] == "R"
-    assert result["Released"] == "31 Mar 1999"
-    assert result["Runtime"] == "136 min"
-    assert result["Genre"] == "Action, Sci-Fi"
-    assert result["Director"] == "Lana Wachowski, Lilly Wachowski"
-    assert result["Writer"] == "Lilly Wachowski, Lana Wachowski"
+
+
+@pytest.fixture
+def sheet():
+    return MagicMock(spec=ezsheets.Sheet)
+
+
+@pytest.fixture
+def ss():
+    return MagicMock(spec=ezsheets.Spreadsheet)
+
+
+def test_read_user_cli_args_add():
+    with patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=MagicMock(add=True, view=False),
+    ):
+        args = read_user_cli_args()
+        assert args.add is True
+        assert args.view is False
+
+
+def test_read_user_cli_args_view():
+    with patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=MagicMock(view=True, add=False),
+    ):
+        args = read_user_cli_args()
+        assert args.add is False
+        assert args.view is True
+
+
+def test_get_movie_data(imdb_id, movie_data):
+    with patch("requests.get", return_value=MagicMock(json=lambda: movie_data)):
+        assert get_movie_data(imdb_id) == movie_data
+
+
+def test_get_movie_data_http_error(imdb_id):
+    with patch("requests.get", side_effect=requests.exceptions.HTTPError):
+        with pytest.raises(SystemExit):
+            get_movie_data(imdb_id)
+
+
+def test_get_movie_data_request_exception(imdb_id):
+    with patch("requests.get", side_effect=requests.exceptions.RequestException):
+        with pytest.raises(SystemExit):
+            get_movie_data(imdb_id)
+
+
+def test_get_movie_data_general_error(imdb_id):
+    with patch("requests.get", side_effect=Exception):
+        with pytest.raises(SystemExit):
+            get_movie_data(imdb_id)
+
+
+def test_get_movie_data_invalid_id(imdb_id):
+    with patch(
+        "requests.get",
+        return_value=MagicMock(json=lambda: {"Response": "False", "Error": "Error"}),
+    ):
+        with pytest.raises(SystemExit):
+            get_movie_data(imdb_id)
+
+
+def test_find_movie_sheet():
+    spreadsheet = MagicMock(spec=ezsheets.Spreadsheet)
+    sheet = MagicMock(title="Sheepy")
+    spreadsheet.sheets = [sheet, MagicMock(title="Other")]
+    assert find_movie_sheet(spreadsheet) == sheet
+
+
+def test_find_movie_sheet_not_found():
+    spreadsheet = MagicMock(spec=ezsheets.Spreadsheet)
+    spreadsheet.sheets = [MagicMock(title="Other"), MagicMock(title="Another")]
+    assert find_movie_sheet(spreadsheet) is None
+
+
+def test_find_free_row():
+    sheet = MagicMock(spec=ezsheets.Sheet)
+    assert find_free_row(sheet) == sheet.rowCount + 1
+
+
+def test_extract_movie_data(movie_data):
+    extracted_data = extract_movie_data(movie_data, watched=True, add=True)
+    assert extracted_data["Watched?"] == "TRUE"
+    assert extracted_data["Title"] == "Inception"
+    assert extracted_data["Year"] == "2010"
+    assert extracted_data["Genre"] == "Action, Adventure, Sci-Fi"
+    assert extracted_data["Runtime"] == "148 min"
+    assert extracted_data["Suggested by"] == SUGGESTED_BY
+    assert extracted_data["IMDb-Rating"] == "8.8"
+    assert extracted_data["Tomatometer"] == "87%"
+    assert extracted_data["Dirctor"] == "Christopher Nolan"
     assert (
-        result["Actors"] == "Keanu Reeves, Laurence Fishburne, "
-        "Carrie-Anne Moss, Hugo Weaving"
+        extracted_data["Plot"]
+        == "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O."
     )
     assert (
-        result["Plot"] == "A computer hacker learns from mysterious "
-        "rebels about the true nature of his reality "
-        "and his role in the war against its controllers."
+        extracted_data["Poster"]
+        == '=IMAGE("https://m.media-amazon.com/images/M/MV5BMjAxMjA5MjgxNF5BMl5BanBnXkFtZTcwMjQyMjIyMw@@._V1_SX300.jpg")'
     )
-    assert result["Language"] == "English"
-    assert result["Country"] == "USA"
-    assert result["Awards"] == "Won 4 Oscars. Another 37 wins & 51 nominations."
-    assert (
-        result["Poster"]
-        == "https://m.media-amazon.com/images/M/MV5BMjAxMjg5ODI4N15BMl5BanBnXkFtZTYwNjM5Mzg5._V1_SX300.jpg"
-    )
-    assert result["Ratings"] == [
-        {"Source": "Internet Movie Database", "Value": "8.7/10"},
-        {"Source": "Rotten Tomatoes", "Value": "88%"},
-        {"Source": "Metacritic", "Value": "73/100"},
-    ]
-    assert result["Metascore"] == "73"
-    assert result["imdbRating"] == "8.7"
-    assert result["imdbVotes"] == "1,719,757"
-    assert result["imdbID"] == "tt0133093"
-    assert result["Type"] == "movie"
-    assert result["DVD"] == "21 Sep 1999"
-    assert result["BoxOffice"] == "$171,479,930"
-    assert result["Production"] == "Village Roadshow Prod., Silver Pictures"
-    assert result["Website"] == "N/A"
-    assert result["Response"] == "True"
-
-
-@patch("sheepy.core.requests.get")
-def test_get_movie_data_http_error(mock_get):
-    mock_get.side_effect = requests.exceptions.HTTPError()
-    with pytest.raises(SystemExit):
-        get_movie_data("tt0133093")
-
-
-@patch("sheepy.core.requests.get")
-def test_get_movie_data_request_exception(mock_get):
-    mock_get.side_effect = requests.exceptions.RequestException()
-    with pytest.raises(SystemExit):
-        get_movie_data("tt0133093")
-
-
-@patch('sheepy.core.requests.get')
-def test_get_movie_data_no_response(mock_get):
-    mock_get.return_value = None
-    with pytest.raises(SystemExit):
-        get_movie_data("tt0133093")
-
-#  def test_find_free_row():
-#      # Create a new spreadsheet and get its first sheet
-#      ss = ezsheets.createSpreadsheet()
-#      sheet = ss[0]
-#  
-#      # Test when the sheet is empty
-#      assert find_free_row(sheet) == 1
-#  
-#      # Test when the sheet has some data
-#      sheet.updateRow(1, ["Name", "Age", "Gender"])
-#      sheet.updateRow(2, ["Alice", "25", "Female"])
-#      sheet.updateRow(3, ["Bob", "30", "Male"])
-#      assert find_free_row(sheet) == 4
-#  
-#      # Test when the sheet is full
-#      for i in range(4, sheet.rowCount + 1):
-#          sheet.updateRow(i, ["aaa", "aaa", "aaa"])
-#      assert find_free_row(sheet) == sheet.rowCount + 1
-#  
-#      # Delete the spreadsheet
-#      ss.delete()
