7df1fb11ccef3784cc21bcb2014dc2baad193530
Jannes <jannes.hachmer@gmail.com>
2024-02-24 01:33:33 +0100

    Made Spreadsheet Class and relevant functions available from top-level package

--- src/sheepy/__init__.py
+++ src/sheepy/__init__.py
@@ -1,3 +1,6 @@
 from dotenv import load_dotenv
 
+from sheepy.omdbapi.omdb import process_movie_request  # noqa: F401
+from sheepy.spreadsheet.spreadsheet import SheepySpreadsheet  # noqa: F401
+
 load_dotenv()
