7f07ffb1f44e3345f8b6509efbc31b5119bede44
Jannes <jannes.hachmer@gmail.com>
2024-07-31 23:49:05 +0200

    moved building of request url to separate function

--- src/sheepy/util/string_util.py
+++ src/sheepy/util/string_util.py
@@ -16,3 +16,24 @@ def insert_newlines(string: str, nth: int = 64) -> str:
     for i in range(0, len(string), nth):
         lines.append(string[i : i + nth])
     return "\n".join(lines)
+
+
+def build_request_url(
+    base_url: str, api_key: str, title_or_id: str, year: int | None = None
+) -> str:
+    """builds requests url for omdb api
+     if year is provieded an url is build based on title and year
+     otherwise url is build using imdb id
+
+    Args:
+        base_url (str): base url for request (https://xyz.com)
+        api_key (str): key for api access
+        title_or_id (str): title or id identifying movie
+        year (int | None, optional): year in which movie was released. Defaults to None.
+
+    Returns:
+        str: returns build url for api request
+    """
+    if year is not None:
+        return base_url + api_key + "&t=" + title_or_id + "&y=" + str(year)
+    return base_url + api_key + "&i=" + title_or_id
