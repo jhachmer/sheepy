20202dc7b6bae0f646717790b6bc816e45af9dc7
Jannes <jannes.hachmer@gmail.com>
2024-07-31 23:47:22 +0200

    added docstring for insert_newlines function

--- src/sheepy/util/string_util.py
+++ src/sheepy/util/string_util.py
@@ -1,8 +1,18 @@
 """Utilities to manipulate strings"""
 
 
-def insert_newlines(string: str, every: int = 64) -> str:
+def insert_newlines(string: str, nth: int = 64) -> str:
+    """inserts newlines at every nth character
+
+    Args:
+        string (str): string to alter
+        every (int, optional): number of occurences when newline is inserted.
+         Defaults to 64.
+
+    Returns:
+        str: returns new string with newlines
+    """
     lines = []
-    for i in range(0, len(string), every):
-        lines.append(string[i: i + every])
+    for i in range(0, len(string), nth):
+        lines.append(string[i : i + nth])
     return "\n".join(lines)
