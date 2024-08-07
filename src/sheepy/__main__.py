5509b864c5805b1c0d463f33beb2da1713974446
Jannes <jannes.hachmer@gmail.com>
2024-08-08 00:05:01 +0200

    package renames

--- src/sheepy/__main__.py
+++ src/sheepy/__main__.py
@@ -1,14 +1,17 @@
-from sheepy.util.cli import read_user_cli_args
+import argparse
+import logging
+
+from sheepy.cli.cli import read_user_cli_args
 from sheepy.util.logger import get_logger
 
 
 def main() -> None:
     """
-    Main entry for cli logic
+    Main entry for application
     """
-    logger = get_logger(__name__)
-    print("Hello there, welcome to Sheepy :)\N{snake}\N{snake}\N{snake}\n")
-    args = read_user_cli_args()
+    logger: logging.Logger = get_logger(__name__)
+    print("\N{snake}\N{snake} Hello, welcome to Sheepy \N{snake}\N{snake}\n")
+    args: argparse.Namespace = read_user_cli_args()
     logger.debug(args)
 
     args.func(args)
