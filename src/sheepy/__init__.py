b2aeeb4f8ebd28d13a055b0a05929abd772b5196
Jannes <jannes.hachmer@gmail.com>
2024-11-10 23:55:26 +0100

    upimported core functions in init file

--- src/sheepy/__init__.py
+++ src/sheepy/__init__.py
@@ -1,3 +1,14 @@
+# ruff: noqa
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+from sheepy.core import (
+    add_movie_to_sheet,
+    create_new_sheet,
+    download_csv,
+    get_env_spreadsheet,
+    view_movie_info,
+    watch_clipboard,
+)
